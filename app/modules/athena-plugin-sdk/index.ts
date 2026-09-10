a741a0e286bd0aa8c830917e74bcd108478cfdea
Billy K Showalter <bshowa4@wgu.edu>
2026-09-10 17:49:06 -0400

    feat(athena): expose plugin manifest SDK

---
+++ app/modules/athena-plugin-sdk/index.ts
@@ -0,0 +1,8 @@
+export { athenaPluginManifestSchema, validateAthenaPluginManifest } from "./manifest";
+export type {
+  AthenaPluginManifest,
+  AthenaPluginManifestReasonCode,
+  AthenaPluginManifestValidationIssue,
+  AthenaPluginManifestValidationResult,
+  AthenaPluginNetworkPolicy,
+} from "./types";
