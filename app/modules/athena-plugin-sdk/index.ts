c2de708f034cb9614af55eaf382bcbdf2acb4e95
Billy K Showalter <bshowa4@wgu.edu>
2026-09-10 17:52:39 -0400

    feat(athena): expose A13 plugin governance API

--- app/modules/athena-plugin-sdk/index.ts
+++ app/modules/athena-plugin-sdk/index.ts
@@ -1,8 +1,17 @@
 export { athenaPluginManifestSchema, validateAthenaPluginManifest } from "./manifest";
+export { createApprovedPluginReview, hashAthenaPluginManifest, reviewMatchesManifest } from "./review";
+export { evaluatePluginCapability, installApprovedPlugin, transitionPluginGrant } from "./lifecycle";
+export { assertPluginEventAllowed, assertPluginNetworkAllowed, assertPluginPermissionAllowed, assertPluginSandboxAllows } from "./sandbox";
+export { assertPluginContextProviderMayRun, assertPluginEventPublicationMayRun, assertPluginEventSubscriptionMayRun, assertPluginMayLoad, assertPluginToolMayExecute } from "./runtime";
 export type {
+  AthenaPluginCapabilityDecision,
+  AthenaPluginGrant,
+  AthenaPluginInstallStatus,
   AthenaPluginManifest,
   AthenaPluginManifestReasonCode,
   AthenaPluginManifestValidationIssue,
   AthenaPluginManifestValidationResult,
   AthenaPluginNetworkPolicy,
+  AthenaPluginReviewRecord,
+  AthenaPluginReviewStatus,
 } from "./types";
